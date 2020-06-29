36a39208d600987c22300f15199833ae0053f9a1
oysbak <64131013+oysbak@users.noreply.github.com>
2020-06-29 22:28:12 +0200

    First commit

---
+++ Problems/Composite numbers/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static boolean isComposite(long number) {
+        // write your code here
+        boolean isComposite = false;
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                isComposite = true;
+                break;
+            }
+        }
+        return isComposite;
+    }
+
+    /* Do not change code below */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        final long number = scanner.nextLong();
+        System.out.println(isComposite(number));
+    }
+}
